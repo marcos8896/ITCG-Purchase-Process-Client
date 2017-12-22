ce51b6646cdddcab86fac2189420bd5a6d131314
Brandon Villa <bornofos@gmail.com>
2017-12-21 18:25:51 -0600

    Components imported automatically

--- src/app/views/warehouse/project/project.module.ts
+++ src/app/views/warehouse/project/project.module.ts
@@ -5,6 +5,10 @@ import { CommonModule } from '@angular/common';
 
 import { ProjectComponent } from './project.component';
 import { ProjectRoutingModule } from './project.routing';
+import { ProjectCreateComponent } from './components/project-create/project-create.component';
+import { ProjectEditComponent } from './components/project-edit/project-edit.component';
+import { ProjectShowComponent } from './components/project-show/project-show.component';
+import { ProjectsComponent } from './components/projects/projects.component';
 
 @NgModule({
   imports: [
@@ -15,6 +19,10 @@ import { ProjectRoutingModule } from './project.routing';
   ],
   declarations: [
     ProjectComponent,
+    ProjectCreateComponent,
+    ProjectEditComponent,
+    ProjectShowComponent,
+    ProjectsComponent,
   ]
 })
 export class ProjectModule { }
