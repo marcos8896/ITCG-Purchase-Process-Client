7ee075209ef2cfde77ee1a4555463cdaecd0e066
Diego Moreno <diegomoreno@Diegos-MacBook-Air.local>
2017-12-21 20:28:22 -0600

    Added the 'Program' module component  at 'wareHouse'

---
+++ src/app/views/warehouse/program/program.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { ChartsModule } from 'ng2-charts/ng2-charts';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+import { ProgramComponent } from './program.component';
+import { ProgramRoutingModule } from './program.routing';
+import { ProgramCreateComponent } from './components/program-create/program-create.component';
+import { ProgramEditComponent } from './components/program-edit/program-edit.component';
+import { ProgramShowComponent } from './components/program-show/program-show.component';
+import { ProgramsComponent } from './components/programs/programs.component';
+
+
+
+@NgModule({
+  imports: [
+    ProgramRoutingModule,
+    ChartsModule,
+    CommonModule,
+    FormsModule
+  ],
+  declarations: [
+    ProgramComponent,
+    ProgramCreateComponent,
+    ProgramEditComponent,
+    ProgramShowComponent,
+    ProgramsComponent,  
+  ]
+})
+export class ProgramModule { }
