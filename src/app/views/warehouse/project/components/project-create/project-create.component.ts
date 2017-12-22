0a089fac3d74deca69ffb38082346ff1459127a6
Brandon Villa <bornofos@gmail.com>
2017-12-21 18:50:46 -0600

    project create is working

--- src/app/views/warehouse/project/components/project-create/project-create.component.ts
+++ src/app/views/warehouse/project/components/project-create/project-create.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
+import { ProjectService } from 'app/services/project.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-project-create',
@@ -6,10 +9,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./project-create.component.scss']
 })
 export class ProjectCreateComponent implements OnInit {
+  @ViewChild('descriptionInput') description: any;
+  @ViewChild('projectNumberInput') projectNumber: any;
 
-  constructor() { }
+  constructor( private projectService: ProjectService, private toastrService: ToastrService ) { }
 
   ngOnInit() {
+  }
+
+  onSubmitProject( values ) {
+    this.projectService.create( values )
+      .subscribe( res => {
+        if ( res ) {
+          this.showSuccess()
+          this.clearInputs()
+        }
+      },
+      data => this.showError(data.error.message),
+      () => console.log('Completed'))
+  }
+
+  showSuccess() {
+    this.toastrService.success('Registro agregado exitosamente', '¡Registro agregado!')
+  }
+
+  showError( error ) {
+    this.toastrService.error(error, '¡Ha numa!')
+  }
+
+  clearInputs() {
+    this.description.nativeElement.value = ''
+    this.projectNumber.nativeElement.value = ''
   }
 
 }
