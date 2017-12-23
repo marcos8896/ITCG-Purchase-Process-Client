1b031f0631e65f17f05b6440923770c8fd01b77d
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-23 14:10:36 -0600

    Implemented providers(all) component.

--- src/app/views/warehouse/provider/components/providers/providers.component.ts
+++ src/app/views/warehouse/provider/components/providers/providers.component.ts
@@ -1,4 +1,7 @@
+import { Provider } from 'app/models/provider';
 import { Component, OnInit } from '@angular/core';
+  
+import { ProviderService } from 'app/services/provider.service';
 
 @Component({
   selector: 'app-providers',
@@ -6,10 +9,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./providers.component.scss']
 })
 export class ProvidersComponent implements OnInit {
+  providers: Provider[] = []
 
-  constructor() { }
+  constructor(private providerService: ProviderService) { }
 
   ngOnInit() {
+    this.providerService.all()
+    .subscribe( res => this.providers = res)   
   }
 
 }
