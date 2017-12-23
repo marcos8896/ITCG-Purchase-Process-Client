08fa4cb4fa045cfd680bdbbfe524de896f07edc2
Brandon Villa <bornofos@gmail.com>
2017-12-23 12:24:14 -0600

    app-session-footer component created

---
+++ src/app/components/app-session-footer/app-session-footer.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-session-footer',
+  templateUrl: './app-session-footer.component.html'
+})
+export class AppSessionFooterComponent { }
