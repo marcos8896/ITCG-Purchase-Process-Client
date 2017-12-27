f23cf05d54be275cc21b702648d0bb831022f10d
Diego Moreno <diegomoreno@Diegos-Air.lan>
2017-12-27 09:53:22 -0600

    Added routing for department components.

---
+++ src/app/views/warehouse/department/department.routing.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DepartmentCreateComponent } from 'app/views/warehouse/department/components/department-create/department-create.component';
+import { DepartmentEditComponent } from 'app/views/warehouse/department/components/department-edit/department-edit.component';
+import { DepartmentShowComponent } from 'app/views/warehouse/department/components/department-show/department-show.component';
+import { DepartmentsComponent } from 'app/views/warehouse/department/components/departments/departments.component';
+
+
+const routes: Routes = [
+  {
+    path: 'create',
+    component: DepartmentCreateComponent,
+    data: {
+      title: 'Registrar departamento'
+    }
+  },
+  {
+    path: 'show',
+    component: DepartmentShowComponent,
+    data: {
+      title: 'Consultar departamento'
+    }
+  },
+  {
+    path: 'edit',
+    component: DepartmentEditComponent,
+    data: {
+      title: 'Editar departamento'
+    }
+  },
+  {
+    path: 'all',
+    component: DepartmentsComponent,
+    data: {
+      title: 'Consulta General'
+    }
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DepartmentRoutingModule {}
