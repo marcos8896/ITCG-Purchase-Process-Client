4eb2124433eb03049eb89d7a52e29ccf1369893a
Diego Moreno <diegomoreno@Diegos-Air.lan>
2017-12-27 09:54:39 -0600

    Added the 'show' module for department.

---
+++ src/app/views/warehouse/department/components/department-show/department-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-department-show',
+  templateUrl: './department-show.component.html',
+  styleUrls: ['./department-show.component.scss']
+})
+export class DepartmentShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
