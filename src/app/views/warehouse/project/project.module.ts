2f2e7a74a1a8231d7616ca72a2784e13b76203d0
Brandon Villa <bornofos@gmail.com>
2017-12-27 23:15:27 -0600

    NgxDatatableModule imported

--- src/app/views/warehouse/project/project.module.ts
+++ src/app/views/warehouse/project/project.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { ChartsModule } from 'ng2-charts/ng2-charts';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
 
 import { ProjectComponent } from './project.component';
 import { ProjectRoutingModule } from './project.routing';
@@ -15,7 +16,9 @@ import { ProjectsComponent } from './components/projects/projects.component';
     ProjectRoutingModule,
     ChartsModule,
     CommonModule,
-    FormsModule
+    FormsModule,
+    NgxDatatableModule
+
   ],
   declarations: [
     ProjectComponent,
