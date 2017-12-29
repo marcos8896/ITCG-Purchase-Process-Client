db4139101a6612c5e93f42bddf25d58bcc60cb6e
Brandon Villa <bornofos@gmail.com>
2017-12-29 14:06:41 -0600

    Login module, routing, template and component created

---
+++ src/app/views/session/login/login.module.ts
@@ -0,0 +1,19 @@
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { LoginComponent } from './login.component';
+import { LoginRoutingModule } from 'app/views/session/login/login.routing';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        LoginRoutingModule
+    ],
+    exports: [],
+    declarations: [ LoginComponent ],
+    providers: [],
+})
+export class LoginModule { }
