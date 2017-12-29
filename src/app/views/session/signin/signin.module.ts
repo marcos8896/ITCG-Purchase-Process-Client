25a724694b1ac2ed0899a830e63e87bb7dc9220a
Brandon Villa <bornofos@gmail.com>
2017-12-28 23:27:22 -0600

    signin layout improved

--- src/app/views/session/signin/signin.module.ts
+++ src/app/views/session/signin/signin.module.ts
@@ -1,10 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin.component';
 import { SigninRoutingModule } from './signin.routing';
 
 @NgModule({
   imports: [
+    CommonModule,
+    FormsModule,
     SigninRoutingModule
   ],
   declarations: [ SigninComponent ]
