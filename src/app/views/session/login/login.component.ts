6cbf1286810a972d0e5a20a672cc2dbc7f616a29
Brandon Villa <bornofos@gmail.com>
2017-12-29 18:48:09 -0600

    login template and component  improved

--- src/app/views/session/login/login.component.ts
+++ src/app/views/session/login/login.component.ts
@@ -1,3 +1,4 @@
+import { AuthenticationService } from 'app/services/authentication.service';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'app/services';
@@ -11,21 +12,30 @@ import { Validators } from '@angular/forms';
 
 export class LoginComponent {
 
-    public loginForm: FormGroup
-
-    constructor( 
-        private formBuilder: FormBuilder,
-        private userService: UserService,
-        private toastrService: ToastrService
-      ) {
-        this.createForm()
-       }
-
-    
-    createForm() {
-        this.loginForm = this.formBuilder.group({
-          username: ['', Validators.compose([Validators.required])],
-          password: ['', Validators.compose([Validators.required])],
-        })
+  public loginForm: FormGroup
+  
+  constructor( 
+      private formBuilder: FormBuilder,
+      private userService: UserService,
+      private toastrService: ToastrService,
+      private authenticationService: AuthenticationService
+    ) {
+      this.createForm()
       }
+  
+  createForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+    })
+  }
+
+  onSubmitLogin( values ) {
+    this.authenticationService.login( values.email, values.password )
+      .subscribe( res => {
+        console.log( res )
+      },
+      error => console.log('Error: ', error),
+      () => console.log('Completed'))
+  }
 }
