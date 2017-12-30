e07ba5cfde268efa536518df3a602f0c2f30984b
Brandon Villa <bornofos@gmail.com>
2017-12-29 18:47:53 -0600

    login method improved

--- src/app/services/authentication.service.ts
+++ src/app/services/authentication.service.ts
@@ -16,25 +16,25 @@ export class AuthenticationService {
         this.endPoint = `${ConfigUrlService.BASE_URL}/Users`
         this.headers = new Headers({ 'Content-Type': 'application/json' })
     }
-    
-    login( username: string, password: string ) {
-        return this.http.post(`${this.endPoint}/login`, { username, password })
-            .map( (response: any) => {
-                console.log(response)
+
+    login( email: string, password: string ) {
+        return this.http.post(`${this.endPoint}/login`, { email, password })
+            .map(( response: any ) => {
+                response = response.json()
                 if ( response && response.id ) {
-                    localStorage.setItem('ITCGToken', JSON.stringify(response.id));
-                    localStorage.setItem('userId', JSON.stringify(response.userId));
+                    localStorage.setItem('ITCG_token', JSON.stringify(response.id));
+                    localStorage.setItem('ITCG_userId', JSON.stringify(response.userId));
                 }
- 
                 return response;
             })
             .catch( this.handleError )
     }
-    
+
     logout() {
-        localStorage.removeItem('ITCGToken');
-        localStorage.removeItem('userId');
+        localStorage.removeItem('ITCG_token');
+        localStorage.removeItem('ITCG_userId');
     }
+    
     private handleError( error: Response ) {
         return Observable.throw( error.json() || 'Server error' )
     }
