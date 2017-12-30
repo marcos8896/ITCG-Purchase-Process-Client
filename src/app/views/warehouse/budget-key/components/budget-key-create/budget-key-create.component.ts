7f68905fc0a021f6201588333a378f29ec809587
KarinGarciaZ <oscarin962011@hotmail.com>
2017-12-30 15:45:26 -0600

    First aproach of budget.key-create.

--- src/app/views/warehouse/budget-key/components/budget-key-create/budget-key-create.component.ts
+++ src/app/views/warehouse/budget-key/components/budget-key-create/budget-key-create.component.ts
@@ -1,4 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { ProgramService } from './../../../../../services/program.service';
+import { SubdirectionService } from './../../../../../services/subdirection.service';
+import { ProjectService } from 'app/services/project.service';
+import { ProgramInterface } from './../../../../../models/program';
+import { getTestBed } from '@angular/core/testing';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { BudgetKeyService} from 'app/services';
+import { ProjectInterface } from 'app/models/project';
+import { Subdirection } from 'app/models/subdirection';
 
 @Component({
   selector: 'app-budget-key-create',
@@ -6,10 +15,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./budget-key-create.component.scss']
 })
 export class BudgetKeyCreateComponent implements OnInit {
+  public project: ProjectInterface;
+  public program: ProgramInterface;
+  public subdirections: Subdirection [];
+  public programs: ProgramInterface [];
+  public tempProjects: ProjectInterface[];
+  public actions = [];
+  @ViewChild('myTable') table: any;
 
-  constructor() { }
+  constructor(
+    private budgetKeyService: BudgetKeyService, 
+    private toastr: ToastrService,
+    private projectService: ProjectService,
+    private subdirectionService: SubdirectionService,
+    private programService: ProgramService
+  ) { }
 
   ngOnInit() {
+    this.getSubdirections()
+    this.getPrograms()
+  }
+
+  getSubdirections(): void {
+    this.subdirectionService.all()
+    .subscribe( data => {
+      this.subdirections = data;
+    });
+  }
+
+  getPrograms(): void {
+    this.programService.getProgramsWithTheirProjects()
+      .subscribe( data => this.programs = data);
+  }
+
+  projectSelected( project, program ) {
+    this.program = program;
+    this.project = project;
+    this.toggleExpandRow( program )
+  }
+
+  onSubmitAddAction( values ) {
+    values.action_number = this.actions.length + 1
+    this.actions.push(values)
+    console.log('this.actions: ', this.actions);
+
+  }
+
+  onSubmitBudgetKey( values ) {
+    values.programId = this.program.id;
+    values.projectId = this.project.id;    
+    this.budgetKeyService.create( values )
+      .subscribe( res => {
+        if ( res ) {
+          this.showSuccess()       
+        }
+      },
+      data => this.showError(data.error.message),
+      () => console.log('Completed'))      
+  }
+
+  toggleExpandRow(row) {
+    this.table.rowDetail.toggleExpandRow(row);
+  }
+  
+  onDetailToggle( event ) {
+    console.log('event: ', event);
+  }
+
+  showSuccess() {
+    this.toastr.success('Registro agregado exitosamente', '¡Registro agregado!')
+  }
+
+  showError( error ) {
+    this.toastr.error(error, '¡Ha numa!')
   }
 
 }
