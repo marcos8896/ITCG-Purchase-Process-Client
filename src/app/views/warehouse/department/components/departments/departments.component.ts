3bc046a0c32a9ebb8e1f052abc56a9180f22caac
Diego Moreno <diegomoreno@Diegos-Air.lan>
2018-01-01 10:30:01 -0600

    Added general query for 'departments'.

--- src/app/views/warehouse/department/components/departments/departments.component.ts
+++ src/app/views/warehouse/department/components/departments/departments.component.ts
@@ -1,4 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Subdirection } from 'app/models/subdirection';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { DatatableComponent } from '@swimlane/ngx-datatable/src/components/datatable.component';
+import 'rxjs/add/operator/debounceTime';
+
+import { SubdirectionService } from 'app/services/subdirection.service';
+import { DepartmentService } from 'app/services/department.service';
+
+
+import { DepartmentInterface } from 'app/models/deparment';
 
 @Component({
   selector: 'app-departments',
@@ -7,9 +17,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DepartmentsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('myTable') table: any;
+ 
+  public departments: DepartmentInterface[];
+  public temp: any = [];
+  public selectedFilter = "name";
+  public filterBy = [ 
+    { value: "id", name: "ID" }, 
+    { value: "name", name: "Nombre del departamento" },
+    { value: "budget", name: "Presupuesto"},
+    { value: "subdirectionid", name: "Subdirección"}
+  ]
+
+  // For debounce purpose
+  private subject: BehaviorSubject<string>;
+  public searchTextValue: string;
+
+  constructor( private departmentService: DepartmentService, private subdirectionService: SubdirectionService) { 
+    this.subject = new BehaviorSubject<string>(this.searchTextValue);
+  }
+
+  someProperty:string = '';
 
   ngOnInit() {
+    this.getDepartments();
+    this.debounce();
+  }
+
+  getDepartments() :void {
+    this.departmentService.allDepartmentRelations()
+      .subscribe( res => {
+        this.temp = [...res];
+        this.departments = res;
+        console.log(this.departments)
+      }); 
+  }
+ 
+  onDetailToggle( event ) {
+    console.log('event: ', event);
+  }
+
+  toggleExpandRow(row) {
+    console.log('Toggled Expand Row!', row);
+    this.table.rowDetail.toggleExpandRow(row);
+  }
+
+  debounce() {
+    this.subject.debounceTime(400).subscribe( searchTextValue => {
+      if ( searchTextValue !== undefined ) {
+        const val = searchTextValue.toLowerCase();
+        
+        // filter our data
+        const temp = this.temp.filter( element => element[this.selectedFilter].toString().toLowerCase().indexOf(val) !== -1 || !val );
+    
+        // update the rows
+        this.departments = temp;
+        // Whenever the filter changes, always go back to the first page
+        this.table.offset = 0;
+      }
+    })
+  }
+
+  updateFilter() {
+    this.subject.next(this.searchTextValue);
   }
 
 }
