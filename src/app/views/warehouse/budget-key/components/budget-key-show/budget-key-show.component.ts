c185c6b1d5ab6b2201b34cbd2d19aec9fb04438a
Diego Moreno <diegomoreno@Diegos-Air.lan>
2018-01-04 08:13:03 -0600

    Added the show component of 'budget key'

---
+++ src/app/views/warehouse/budget-key/components/budget-key-show/budget-key-show.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-budget-key-show',
+  templateUrl: './budget-key-show.component.html',
+  styleUrls: ['./budget-key-show.component.scss']
+})
+export class BudgetKeyShowComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
