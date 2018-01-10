3fcd97f74f959bc1963a6f2f625fd7963e0399f7
Marcos Barrera Del Río <marcos@softwareallies.com>
2018-01-09 19:46:24 -0600

    Used 'findByIdCustomFilters' method instead of the previous URL REST method.

--- src/app/services/budget-key.service.ts
+++ src/app/services/budget-key.service.ts
@@ -15,10 +15,5 @@ export class BudgetKeyService extends BasicRequestService {
       .map(res => res.json() || { }, console.log(this.endPoint))
       .catch(err => JSON.stringify(err));
   }
-
-  public getBudgetKeyWithItsDetails( budgetKeyId ): Observable<any[]> { 
-    return this.http.get(`${this.endPoint}/${ budgetKeyId }?filter[include]=budget_key_details&filter[include]=subdirection&filter[include]=program&filter[include]=project`)
-      .map(res => res.json() || { })
-      .catch(err => JSON.stringify(err));
-  }
+  
 }
