87fb4484bdb68bd0b900da24c22069d8024e308e
Marcos Barrera Del Río <marcos@softwareallies.com>
2018-01-09 19:45:19 -0600

    Added 'findByIdCustomFilters' and 'getAll' to the basic-request in order to use all the Loopback capabilities from the Angular Client.

--- src/app/services/basic-request.service.ts
+++ src/app/services/basic-request.service.ts
@@ -34,6 +34,56 @@ export class BasicRequestService {
       .catch( this.handleError );
   }
 
+  /**
+   * Get all the records that match with the given query params.
+   * 
+   * @param { Object } [where={}] Loopback's where syntax - Example: { where : {property: "text to match"} }
+   * @param { string | string[] } [include={}] Loopback's include - Example: "myrelation1" | ["myrelation1", "myrelation2"] => Get records with the related objects.
+   * @param { number } [limit=25] Loopback's limit - Example: 10 => Get maximum 10 records.
+   * @param { number } [skip=0] Loopback's skip - Example: 10 => Skip first 10 records.
+   * @param { string | string[] } [order=null] Loopback's order - Example: "property ASC" or ["property ASC", "property2 DESC"] => Get records with the wanted order.
+   * @param { Object } [fields={}] Loopback's fields - Example: { property1: true, property2: true } => Get records only with two properties.
+   * @returns { Observable<any> } 
+   * @memberof BasicRequestService
+   */
+  public getAll( where: any = {}, include: string[] | string = [], limit: number = 25, 
+                skip: number = 0, order: string[] | string = [], fields: any = {} ): Observable<any> {
+
+    return this.http.get(`${this.endPoint}`, {
+      headers: this.headers,
+      params: {
+        filter: { where, limit, skip, order, fields, include }
+      }
+    })
+      .map( res => res.json() || {} )
+      .catch( this.handleError );
+  }
+
+  /**
+   * Get a record based on its id and the given query params.
+   * 
+   * @param { number } [id=null] Model's ID.
+   * @param { Object } [where={}] Loopback's where syntax - Example: { where : {property: "text to match"} }
+   * @param { string | string[] } [include={}] Loopback's include - Example: "myrelation1" | ["myrelation1", "myrelation2"] => Get records with the related objects.
+   * @param { number } [limit=25] Loopback's limit - Example: 10 => Get maximum 10 records.
+   * @param { number } [skip=0] Loopback's skip - Example: 10 => Skip first 10 records.
+   * @param { string | string[] } [order=null] Loopback's order - Example: "property ASC" or ["property ASC", "property2 DESC"] => Get records with the wanted order.
+   * @param { Object } [fields={}] Loopback's fields - Example: { property1: true, property2: true } => Get records only with two properties.
+   * @returns { Observable<any> } 
+   * @memberof BasicRequestService
+   */
+  public findByIdCustomFilters( id: any = null, where: any = {}, include: string[] | string = [], limit: number = 25, 
+                                skip: number = 0, order: string[] | string = [], fields: any = {} ): Observable<any> {
+    return this.http.get(`${this.endPoint}/${id}`, {
+      headers: this.headers,
+      params: {
+        filter: { where, include, limit, skip, order, fields }
+      }
+    })
+      .map( res => res.json() || {} )
+      .catch( this.handleError );
+  }
+
   public update( obj ): Observable<any> {
     return this.http.put(`${this.endPoint}/${obj.id}`, obj, { headers: this.headers })
       .map( res => res.json() || {} )
