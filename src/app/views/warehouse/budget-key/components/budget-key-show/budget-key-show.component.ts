6b6fd0261ac53a4affbe8a4b37af82c30c24a368
Diego Moreno <diegomoreno@Diegos-MacBook-Air.local>
2018-01-09 18:05:30 -0600

    Added the 'description' of program and 'project' into the html of budgetKeyShow component.

--- src/app/views/warehouse/budget-key/components/budget-key-show/budget-key-show.component.ts
+++ src/app/views/warehouse/budget-key/components/budget-key-show/budget-key-show.component.ts
@@ -34,27 +34,12 @@ export class BudgetKeyShowComponent implements OnInit {
       this.id = params['id']
       if( this.id )
         this.getBudgetKeyWithItsDetails();
-      //Tío, este código no es necesario, porque al hacer la request con el método de getDetail, ya te traes
-      //el Budget_Key con todos sus detalles. Lo puedes borrar gg.
-      // if ( this.id ) {
-      //   this.budgetKeyService.findById( this.id )
-      //   .subscribe( budgetKey => {
-      //     this.budgetKey = budgetKey
-      //     console.log('this.budgetKey: ', this.budgetKey);
-      //   } )
-      // }
     })
   }
 
   getBudgetKeyWithItsDetails(): void {
-    //Cambié el url de la request por el formato adecuado. Aunque esta solución es provisional,
-    //porque la manera más adecuada es mandarle parámetros, pero luego lo hacemos bien ;)
     this.budgetKeyService.getBudgetKeyWithItsDetails( this.id )
       .subscribe( data => {
-        //Aquí viene el objeto Budget_Key junto con sus detalles gg
-
-        //Agregué en la interfaz de budgetKey una propiedad opcional llamada 'budgetKeyDetails?: any []'
-        //Para que la cheques ;*
         this.budgetKey = data;
         console.log('this.budgetKey: ', this.budgetKey);
     });
