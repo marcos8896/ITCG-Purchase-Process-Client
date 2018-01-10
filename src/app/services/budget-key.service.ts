14580015109ae4bd94ca30a76a9ea7e3addd00d6
Diego Moreno <diegomoreno@Diegos-MacBook-Air.local>
2018-01-09 18:01:27 -0600

    Added the include of 'program' and 'project' into budget key services.

--- src/app/services/budget-key.service.ts
+++ src/app/services/budget-key.service.ts
@@ -17,7 +17,7 @@ export class BudgetKeyService extends BasicRequestService {
   }
 
   public getBudgetKeyWithItsDetails( budgetKeyId ): Observable<any[]> { 
-    return this.http.get(`${this.endPoint}/${ budgetKeyId }?filter[include]=budget_key_details`)
+    return this.http.get(`${this.endPoint}/${ budgetKeyId }?filter[include]=budget_key_details&filter[include]=subdirection&filter[include]=program&filter[include]=project`)
       .map(res => res.json() || { })
       .catch(err => JSON.stringify(err));
   }
