383304d49f18ca62ea07b40ce726b9d0e3aeca0f
Brandon Villa <bornofos@gmail.com>
2018-01-12 16:57:05 -0600

    Interface for specifyng filter properties for requests

---
+++ src/app/models/filter-properties.ts
@@ -0,0 +1,20 @@
+  /**
+   * Get all the records that match with the given query params.
+   * 
+   * @param { Object } where Loopback's where syntax - Example: { where : {property: "text to match"} }
+   * @param { string | string[] } include Loopback's include - Example: "myrelation1" | ["myrelation1", "myrelation2"] => Get records with the related objects.
+   * @param { number } limit Loopback's limit - Example: 10 => Get maximum 10 records.
+   * @param { number } skip Loopback's skip - Example: 10 => Skip first 10 records.
+   * @param { string | string[] } order Loopback's order - Example: "property ASC" or ["property ASC", "property2 DESC"] => Get records with the wanted order.
+   * @param { Object } fields Loopback's fields - Example: { property1: true, property2: true } => Get records only with two properties.
+   * @returns { Observable<any> } 
+   * @memberof BasicRequestService
+   */
+export interface FilterPropertiesInterface {
+    where?: any,
+    include?: string | string[],
+    limit?: number,
+    skip?: number,
+    order?: string | string[],
+    fields?: any
+}
