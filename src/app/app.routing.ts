3f152c3f81ab745d797c3b0794de858f79e0eac6
Brandon Villa <bornofos@gmail.com>
2018-01-16 16:14:29 -0600

    Access for warehouse only for PLANNING_DEPARTMENT guard & access for dashboard from any role logged

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,3 +1,4 @@
+import { GUARDS } from './guards/_guards';
 import { LoggedUserGuard } from './guards/logged-user.guard';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
@@ -35,13 +36,24 @@ export const routes: Routes = [
     data: {
       title: 'Home'
     },
-    canActivate:  [ LoggedUserGuard ],
     children: [
       {
+        data: {
+          guards: [
+            GUARDS.BOSS_DEPARTMENT,
+            GUARDS.PLANNING_DEPARTMENT,
+            GUARDS.VICE_PRINCIPAL,
+          ]
+        },
+        canActivate:  [ LoggedUserGuard ],
         path: 'dashboard',
         loadChildren: './views/dashboard/dashboard.module#DashboardModule'
       },
       {
+        data: {
+          guards: [ GUARDS.PLANNING_DEPARTMENT ]
+        },
+        canActivate:  [ LoggedUserGuard ],
         path: 'warehouse',
         loadChildren: './views/warehouse/warehouse.module#WarehouseModule'
       }
