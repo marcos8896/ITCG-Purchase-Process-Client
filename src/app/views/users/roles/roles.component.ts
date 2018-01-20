0887f0a49c83eb6e802e7d776775bdce618be4ec
Brandon Villa <bornofos@gmail.com>
2018-01-20 17:53:14 -0600

    methods to fetch list of boss_departments and vice_principals

--- src/app/views/users/roles/roles.component.ts
+++ src/app/views/users/roles/roles.component.ts
@@ -1,11 +1,41 @@
+import { BossDepartmentInterface } from 'app/models/boss-department';
+import { VicePrincipalInterface } from './../../../models/vice-principal';
+import { VicePrincipalService } from 'app/services/vice-principal.service';
+import { BossDepartmentService } from 'app/services/boss-department.service';
 import { Component, OnInit } from '@angular/core';
+import { ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-roles',
     templateUrl: 'roles.component.html'
 })
 
-export class RolesComponent {
-    constructor() { }
+export class RolesComponent implements OnInit {
+    public bossDepartments: BossDepartmentInterface[];
+    public vicePrincipals: VicePrincipalInterface[];
+    constructor(
+        private bossDepartmentService: BossDepartmentService,
+        private vicePrincipalService: VicePrincipalService
+    ) { }
+
+    ngOnInit() {
+        this.getAllVicePrincipals()
+        this.getAllBosses()
+    }
+
+    getAllBosses() {
+        this.bossDepartmentService.getAll()
+            .subscribe( data => {
+                this.bossDepartments = data;
+        })
+    }
+
+    getAllVicePrincipals() {
+        this.vicePrincipalService.getAll()
+            .subscribe( data => this.vicePrincipals = data)
+    }
+
+
+
 
 }
