8e458d6cf0f29c67f3f06cd95478453cd0ae495a
Brandon Villa <bornofos@gmail.com>
2018-01-20 17:54:29 -0600

    NgxDatatableModule imported for table management UserTableComponent added to declarations

--- src/app/views/users/roles/roles.module.ts
+++ src/app/views/users/roles/roles.module.ts
@@ -2,16 +2,19 @@ import { CommonModule } from '@angular/common';
 import { RolesService } from './services/roles.service';
 import { NgModule } from '@angular/core';
 import { RolesRoutingModule } from './roles.routing';
-
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { UserTableComponent } from 'app/views/users/roles/components/user-table/user-table.component';
 import { RolesComponent } from './roles.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        RolesRoutingModule
+        RolesRoutingModule,
+        NgxDatatableModule
     ],
     declarations: [
-        RolesComponent
+        RolesComponent,
+        UserTableComponent
     ],
     providers: [
         RolesService
