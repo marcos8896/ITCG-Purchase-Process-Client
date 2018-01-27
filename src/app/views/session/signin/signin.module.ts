6efa9f23ea916d022168d0464c99ab17bdfb32a8
Brandon Villa <bornofos@gmail.com>
2018-01-27 13:07:15 -0600

    SessionSharedModule imported in LoginModule and SigninModule

--- src/app/views/session/signin/signin.module.ts
+++ src/app/views/session/signin/signin.module.ts
@@ -4,13 +4,15 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { SigninComponent } from './signin.component';
 import { SigninRoutingModule } from './signin.routing';
+import { SessionSharedModule } from 'app/views/session/shared/session-shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    SigninRoutingModule
+    SigninRoutingModule,
+    SessionSharedModule
   ],
   declarations: [
     SigninComponent,
