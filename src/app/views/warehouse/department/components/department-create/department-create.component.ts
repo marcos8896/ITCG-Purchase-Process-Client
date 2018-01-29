2a19e4fe59949915b3a6b0bb21317d5811fca162
Diego Moreno <diegomoreno@Diegos-Air.lan>
2018-01-28 21:01:45 -0600

    Added the method 'getBosses' on 'department-create' and 'department-edit'.

--- src/app/views/warehouse/department/components/department-create/department-create.component.ts
+++ src/app/views/warehouse/department/components/department-create/department-create.component.ts
@@ -1,8 +1,11 @@
-import { SubdirectionService } from 'app/services/subdirection.service';
 import { Component, OnInit } from '@angular/core';
 import { ToastrService } from 'ngx-toastr'
 import { ViewChild } from '@angular/core';
+
 import { DepartmentService } from 'app/services/department.service';
+import { SubdirectionService } from 'app/services/subdirection.service';
+import { BossDepartmentService } from 'app/services/boss-department.service';
+
 import { SubdirectionModule } from 'app/views/warehouse/subdirection/subdirection.module';
 import { SubdirectionComponent } from 'app/views/warehouse/subdirection/subdirection.component';
 
@@ -14,21 +17,28 @@ import { SubdirectionComponent } from 'app/views/warehouse/subdirection/subdirec
 export class DepartmentCreateComponent implements OnInit {
   @ViewChild('nombreInput') name: any;
   @ViewChild('moneyInput') number: any;
-  @ViewChild('selectInput') Selection: any;
+  @ViewChild('selectInput, selectBoss') Selection: any;
   // Subdirections array
   public subdirections;
+  // Bosses array
+  public bosses
   constructor( 
     private departmentService: DepartmentService, 
     private subdirectionService: SubdirectionService, 
+    private bossDepartment: BossDepartmentService,
     private toastr: ToastrService ) {
       this.subdirectionService.getAll()
-      .subscribe(subdirection => this.subdirections = subdirection)
+      .subscribe( subdirection => this.subdirections = subdirection )
+      
   }
 
   ngOnInit() {
-
+    this.getBosses()
   }
-
+  getBosses(){
+    this.bossDepartment.getAll()
+    .subscribe( bosses  => this.bosses = bosses )
+  }
   onSubmitDepartment( values ) {
     this.departmentService.create( values )
     .subscribe( res => {
