dc78637797a8d4cab136595e37b65218d8c352d3
Marcos Barrera Del Río <marcos@softwareallies.com>
2018-02-06 18:56:54 -0600

    #17 - Removed logout functionality from app-aside-component

--- src/app/components/app-aside/app-aside.component.ts
+++ src/app/components/app-aside/app-aside.component.ts
@@ -1,38 +1,7 @@
-import { ToastrService } from 'ngx-toastr';
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
-import { AuthenticationService } from 'app/services/authentication.service';
 
 @Component({
   selector: 'app-aside',
   templateUrl: './app-aside.component.html'
 })
-export class AppAsideComponent {
-
-  constructor(
-    private authenticationService: AuthenticationService,
-    private toastrService: ToastrService,
-    private router: Router
-  ) { }
-  
-  logout() {
-    this.authenticationService.logout()
-      .then( message => {
-        this.showSuccess( message )
-        this.router.navigate(['/login']);
-      })
-      .catch( error => {
-        this.showError( "Sesión finalizada ;)" )
-        this.router.navigate(['/login']);
-      })
-  }
-  
-  showSuccess( message ) {
-    this.toastrService.success(message, 'Sesión terminada')
-  }
-
-  showError( error ) {
-    this.toastrService.warning(error, '¡Ha numa!')
-  }
-
-}
+export class AppAsideComponent { }
