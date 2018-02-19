5c8fbc7ea63b278114edbf595d0dbc9c23e8cf19
KarinGarciaZ <oscarin962011@hotmail.com>
2018-02-19 09:15:26 -0600

    Added the beggining of the acceptance.

--- src/app/views/acceptances/vice/components/vice-requisitions/vice-requisitions.component.ts
+++ src/app/views/acceptances/vice/components/vice-requisitions/vice-requisitions.component.ts
@@ -1,3 +1,6 @@
+import { DepartmentService } from 'app/services/department.service';
+import { SubdirectionService } from 'app/services/subdirection.service';
+import { VicePrincipalService } from 'app/services/vice-principal.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +10,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViceRequisitionsComponent implements OnInit {
 
-  constructor() { }
+  public requis: any[];
+
+  constructor(
+    private vicePrincipalService: VicePrincipalService,
+    private subdirectionsService: SubdirectionService,
+    private departmentService: DepartmentService
+  ) { }
 
   ngOnInit() {
+    this.getSubdirection()
+  }
+
+  getSubdirection(){
+    this.vicePrincipalService.findById(
+      JSON.parse(localStorage.getItem("ITCG_userId")), { 
+        include: ['subdirection'] 
+      }).subscribe( res => {
+        console.log("res:     ", res) 
+        this.subdirectionsService.findById(res.subdirection.id, {
+          include: ['department']
+        }).subscribe( res2 =>{
+          console.log('res2: ', res2);
+          
+        })
+      })
   }
 
 }
