87e0eaa1edd239edd067a9daa6cf8702ee79fa79
marcos8896 <elyomarcos@gmail.com>
2018-03-24 14:24:21 -0600

    #19 - Added first approach to have a selectable table.

--- src/app/views/processes/purchase-order/components/purchase-order-create/purchase-order-create.component.ts
+++ src/app/views/processes/purchase-order/components/purchase-order-create/purchase-order-create.component.ts
@@ -12,4 +12,45 @@ export class PurchaseOrderCreateComponent implements OnInit {
   ngOnInit() {
   }
 
+  selected = [];
+
+  columns: any[] = [
+    { prop: 'name'} , 
+    { name: 'Company' }, 
+    { name: 'Gender' }
+  ];
+
+  data = [
+      {
+      "name": "Ethel Price",
+      "gender": "female",
+      "company": "Johnson, Johnson and Partners, LLC CMP DDC",
+      "age": 22
+      },
+      {
+      "name": "Claudine Neal",
+      "gender": "female",
+      "company": "Sealoud",
+      "age": 55
+      },
+      {
+      "name": "Beryl Rice",
+      "gender": "female",
+      "company": "Velity",
+      "age": 67
+      },
+      {
+      "name": "Wilder Gonzales",
+      "gender": "male",
+      "company": "Geekko"
+      },
+    ]
+
+    onSelect({ selected }) {
+      console.log('Select Event', selected, this.selected);
+    }
+
+    
+
+
 }
