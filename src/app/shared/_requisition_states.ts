7e6f10d90e5799c74496a5b5e61a7d8209cdf85b
marcos8896 <elyomarcos@gmail.com>
2018-03-25 17:13:49 -0600

    #19 - Added constants for the Requisition's states.

---
+++ src/app/shared/_requisition_states.ts
@@ -0,0 +1,8 @@
+export const REQUISITION_STATES = {
+  CANCELADA: 'Cancelado',
+  ESPERANDO: 'Esperando',
+  ACEPTADA: 'Aceptado',
+  ORDENADA: 'Ordenado',
+  FACTURADA: 'Facturado',
+  RECIBIDA: 'Recibido'
+}
