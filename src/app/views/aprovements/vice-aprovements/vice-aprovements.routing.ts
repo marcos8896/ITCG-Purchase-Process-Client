699c6349a64879b3bb9f8062acf55186d83998d7
Brandon Villa <bornofos@gmail.com>
2018-03-26 12:09:31 -0600

    GUARDS added

--- src/app/views/aprovements/vice-aprovements/vice-aprovements.routing.ts
+++ src/app/views/aprovements/vice-aprovements/vice-aprovements.routing.ts
@@ -1,10 +1,20 @@
+import { LoggedUserGuard } from './../../../guards/logged-user.guard';
+import { GUARDS } from './../../../guards/_guards';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { ViceAprovementsComponent } from './vice-aprovements.component';
 
 const routes: Routes = [
-  { path: '', component: ViceAprovementsComponent },
+  { 
+    path: '',
+    component: ViceAprovementsComponent,
+
+    data: {
+      guards: [ GUARDS.VICE_PRINCIPAL ]
+    },
+    canActivate:  [ LoggedUserGuard ]
+  },
 ];
 
 @NgModule({
