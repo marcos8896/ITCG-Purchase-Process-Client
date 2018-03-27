26ba48a73d2cfa56b00d852f80986ec2291a69b9
Brandon Villa <bornofos@gmail.com>
2018-03-27 10:40:23 -0600

    Module and components importation

--- src/app/views/aprovements/vice-aprovements/vice-aprovements.module.ts
+++ src/app/views/aprovements/vice-aprovements/vice-aprovements.module.ts
@@ -1,14 +1,29 @@
-import { ViceAprovementsRoutingModule } from './vice-aprovements.routing';
 import { NgModule } from '@angular/core';
+import { ViceAprovementsRoutingModule } from './vice-aprovements.routing';
 
+// Custom Components
 import { ViceAprovementsComponent } from './vice-aprovements.component';
+import { CollapseDepartmentComponent } from './components/collapse-departmen/collapse-departments.component';
+
+// Modules
+import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 
 @NgModule({
     imports: [
-        ViceAprovementsRoutingModule
+        ViceAprovementsRoutingModule,
+        CommonModule,
+        FormsModule,
+        NgxDatatableModule,
+        CollapseModule.forRoot()
     ],
     exports: [],
-    declarations: [ViceAprovementsComponent],
+    declarations: [
+        ViceAprovementsComponent,
+        CollapseDepartmentComponent
+    ],
     providers: [],
 })
 export class ViceAprovementsModule { }
